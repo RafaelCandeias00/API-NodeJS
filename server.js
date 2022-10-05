3a4a88d4daecc0861cf8aa76eea93d256a61b33f
Rafael Candeias <rafaelcandeias39@gmail.com>
2022-10-05 14:55:55 -0300

    @RafaelCandeias00 [Task: Criando um servidor Web] {05/10/2022 14:55}

--- server.js
+++ server.js
@@ -1,3 +1,28 @@
 'use strict'
 
-console.log('testando...');
+// Importando módulos
+const http = require('http');
+const debug = require('debug')('nodestr:server');
+const express = require('express');
+
+// Definindo porta da aplicação
+const app = express();
+const port = 3000;
+app.set('port', port);
+
+// Criando servidor
+const server = http.createServer(app);
+const router = express.Router(); // Arquivo de rotas
+
+// Configurando de rota inicial
+const route = router.get('/', (req, res, next) => {
+    res.status(200).send({
+        title: "Node Store API",
+        version: "0.0.1"
+    });
+});
+app.use('/', route);
+
+// Para o servidor ouvir a porta
+server.listen(port);
+console.log('Ouvindo na porta: ' + port)
