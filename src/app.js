a1d7853ffb12221f00897d0892f6056632133eb1
Rafael Candeias <rafaelcandeias39@gmail.com>
2022-10-10 15:35:44 -0300

    @RafaelCandeias00 [Task: criando métodos CRUD] {10/10/2022 15:35}

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 // Importando módulos
 const express = require('express');
+const bodyParser = require('body-parser');
 
 // Definindo porta da aplicação
 const app = express();
 const router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Configurando de rota inicial
 const route = router.get('/', (req, res, next) => {
@@ -12,6 +16,29 @@ const route = router.get('/', (req, res, next) => {
         version: "0.0.1"
     });
 });
+
+// Método CRUD - CREATE
+const create = router.post('/', (req, res, next) => {
+    res.status(201).send(req.body); // Configurando resposta do servidor e pegando corpo da requisição
+});
+
+// Método CRUD - UPDATE
+const put = router.put('/:id', (req, res, next) => {
+    const id = req.params.id; // Recuperando id passado pela rota
+    res.status(200).send({
+        id: id, 
+        item: req.body 
+    });
+});
+
+const del = router.delete('/', (req, res, next) => {
+    res.status(200).send(req.body);
+});
+
+// Rotas que pode ser utilizadas
 app.use('/', route);
+app.use('/products', create);
+app.use('/products', put);
+app.use('/products', del);
 
 module.exports = app;
