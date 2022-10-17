0131da2ab613239fa0a3b294d35adeab4f67b17a
Rafael Candeias <rafaelcandeias39@gmail.com>
2022-10-17 11:26:02 -0300

    @RafaelCandeias00 [Task: add pasta models] {17/10/2022 11:25}

---
+++ src/models/product.js
@@ -0,0 +1,39 @@
+'use strict';
+
+// Criando esquema 
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const shcema = new Schema({
+    title:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    slug:{
+        type: String,
+        required: true,
+        trim: true,
+        index: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price:{
+        type: Number,
+        required: true
+    },
+    active:{
+        type: Boolean,
+        required: true,
+        default: true
+    },
+    tags:[{
+        type: String,
+        required: true
+    }]
+});
+
+module.exports = mongoose.model('Product', shcema);
