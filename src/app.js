a6d32d68ea89724e15ad32094a2755488ee193a0
Rafael Candeias <rafaelcandeias39@gmail.com>
2022-10-21 15:30:03 -0300

    @RafaelCandeias00 [Task: add arquivo de configuração] {21/10/2022 15:30}

--- src/app.js
+++ src/app.js
@@ -2,13 +2,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const config = require('./config');
 
 // Definindo porta da aplicação
 const app = express();
 const router = express.Router();
 
 // Conecta ao banco
-mongoose.connect('mongodb+srv://rafael:rafael@ndstr.q9ms4nb.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connect(config.connectionString);
 
 // Carrega models
 const Product = require('./models/product');
---
+++ src/config.js
@@ -0,0 +1,8 @@
+global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
+global.EMAIL_TMPL = '<strong>{0}</strong>';
+
+module.exports = {
+    connectionString: 'mongodb+srv://rafael:rafael@ndstr.q9ms4nb.mongodb.net/?retryWrites=true&w=majority',
+    sendgridKey: 'TBD',
+    containerConnectionString: 'TBD'
+}
