efb82203a8ed5dfa7e50cc23ad99b01fd0fdd6c6
Rafael Candeias <rafaelcandeias39@gmail.com>
2022-10-24 16:12:02 -0300

    @RafaelCandeias00 [Task: enviando e-mail de boas vindas] {24/10/2022 16:11}

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
-global.EMAIL_TMPL = '<strong>{0}</strong>';
+global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
 
 module.exports = {
     connectionString: 'mongodb+srv://rafael:rafael@ndstr.q9ms4nb.mongodb.net/?retryWrites=true&w=majority',
-    sendgridKey: 'TBD',
+    sendgridKey: 'TBD', // Colocar chave fornecida pelo sendgrid
     containerConnectionString: 'TBD'
 }
